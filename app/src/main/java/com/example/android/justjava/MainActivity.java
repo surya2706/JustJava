9fa7afcca6c398b3b34756f34e9f576d0f5115d0
Karthik <karthik@FULL208-PRT.FULL.Creative>
2017-10-03 22:09:20 +0530

    increment and decrement methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +20,38 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void submitOrder(View view) {
-        display(1);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
+    /*
+    * This method increments the quantity value on pressing + button
+    * */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+    /*
+    * This methods decrements the quantity value on pressing - button
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
     /*
     This method displays the given quantity value on the screen
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
