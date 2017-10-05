ae49d2ddf909ccb25c728f16f37885092e1c88a1
Karthik <karthik@FULL208-PRT.FULL.Creative>
2017-10-05 14:22:05 +0530

    add price message

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void submitOrder(View view){
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank you";
+        displayMessage(priceMessage);
+//        displayPrice(quantity * 5);
     }
 
     /*
@@ -53,5 +57,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
